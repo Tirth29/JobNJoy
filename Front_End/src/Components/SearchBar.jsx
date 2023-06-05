b6ee98ba6501dbc8b53e19c42db13407f047c446
Tirth <tirthsolanki29923@gmail.com>
2023-06-05 21:08:30 +0530

    search

--- Front_End/src/Components/SearchBar.jsx
+++ Front_End/src/Components/SearchBar.jsx
@@ -1,72 +1,16 @@
-import { useState } from "react";
-
 const SearchBar = () => {
-  const [searchInput, setSearchInput] = useState("");
-
-  const countries = [
-    { name: "Belgium", continent: "Europe" },
-    { name: "India", continent: "Asia" },
-    { name: "Bolivia", continent: "South America" },
-    { name: "Ghana", continent: "Africa" },
-    { name: "Japan", continent: "Asia" },
-    { name: "Canada", continent: "North America" },
-    { name: "New Zealand", continent: "Australasia" },
-    { name: "Italy", continent: "Europe" },
-    { name: "South Africa", continent: "Africa" },
-    { name: "China", continent: "Asia" },
-    { name: "Paraguay", continent: "South America" },
-    { name: "Usa", continent: "North America" },
-    { name: "France", continent: "Europe" },
-    { name: "Botswana", continent: "Africa" },
-    { name: "Spain", continent: "Europe" },
-    { name: "Senegal", continent: "Africa" },
-    { name: "Brazil", continent: "South America" },
-    { name: "Denmark", continent: "Europe" },
-    { name: "Mexico", continent: "South America" },
-    { name: "Australia", continent: "Australasia" },
-    { name: "Tanzania", continent: "Africa" },
-    { name: "Bangladesh", continent: "Asia" },
-    { name: "Portugal", continent: "Europe" },
-    { name: "Pakistan", continent: "Asia" },
-  ];
-
-  const handleChange = (e) => {
-    e.preventDefault();
-    setSearchInput(e.target.value);
-  };
-
-  if (searchInput.length > 0) {
-    countries.filter((country) => {
-      return country.name.match(searchInput);
-    });
-  }
-
   return (
-    <div>
-      <div className="flex items-center m-3">
-        <div className="flex border  border-purple-200 rounded">
-          <input
-            type="text"
-            className="block px-4 py-2 w-64 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
-            placeholder="Search..."
-          />
-          <button className="px-4 ml-1 text-white bg-purple-600 border-l rounded ">
-            Search
-          </button>
-        </div>
+    <div className="flex items-center m-3">
+      <div className="flex border  border-purple-200 rounded">
+        <input
+          type="text"
+          className="block px-4 py-2 w-64 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
+          placeholder="Search..."
+        />
+        <button className="px-4 ml-1 text-white bg-purple-600 border-l rounded ">
+          Search
+        </button>
       </div>
-
-      <table>
-
-        {countries.map((country) => {
-          <div>
-            <tr>
-              <td>{country.name}</td>
-              <td>{country.continent}</td>
-            </tr>
-          </div>;
-        })}
-      </table>
     </div>
   );
 };
