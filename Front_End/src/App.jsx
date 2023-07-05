290bb2e3b7c7ca6691dbb3f332b786a6aae675a1
princeninama <princeninama254@gmail.com>
2023-07-05 20:39:11 +0530

    combined Register' q

--- Front_End/src/App.jsx
+++ Front_End/src/App.jsx
@@ -3,26 +3,24 @@ import Sample from "./Components/Sample";
 import Footer from "./Components/Footer";
 import SearchBar from "./Components/SearchBar";
 import { Login } from "./Components/Login";
-import { Register } from "./Components/Register";
+import Register from "./Components/Register";
 import { Forget } from "./Components/Forget";
-import Register2 from "./Components/Register2";
-import Home from "./Components/Home";
+// import Home from "./Components/Home";
 function App() {
   return (
     <>
       <div>
         {/* <SearchBar /> */}
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Register />} />
           <Route path="/home" element={<Sample />} />
           <Route path="/opportunity" element={<Sample />} />
           <Route path="/post" element={<Sample />} />
           <Route path="/refreshment" element={<Sample />} />
           <Route path="/profile" element={<Sample />} />
-          <Route path="/Register" element={<Register />} />
+          {/* <Route path="/Register" element={<Register />} /> */}
           {/* <Route path="/Login" element={<Login />} /> */}
           <Route path="/Forget" element={<Forget />} />
-          <Route path="/Register2" element={<Register2 />} />
         </Routes>
         {/* <Footer />  */}
         {/* <Login /> */}
--- Front_End/src/Components/Register.jsx
+++ Front_End/src/Components/Register.jsx
@@ -1,119 +1,241 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+
 import { useNavigate } from "react-router-dom";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import LockIcon from "@mui/icons-material/Lock";
 import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+import AddPhotoAlternateIcon from "@mui/icons-material/AddPhotoAlternate";
 
-export const Register = () => {
+const Register = () => {
+  const [currentSlide, setCurrentSlide] = useState(0);
   const navigate = useNavigate();
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
+  const [profilePicture, setProfilePicture] = useState(null);
 
+  const handleProfilePictureChange = (e) => {
+    const file = e.target.files[0];
+    setProfilePicture(URL.createObjectURL(file));
+  };
   const handleUsernameChange = (e) => {
-    setUsername(e.target.value);
-  };
-
-  const handleEmailChange = (e) => {
-    setEmail(e.target.value);
-  };
-
-  const handlePasswordChange = (e) => {
-    setPassword(e.target.value);
-  };
-
-  const handleConfirmPasswordChange = (e) => {
-    setConfirmPassword(e.target.value);
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    if (password.trim() !== confirmPassword.trim()) {
-      alert("Confirmed password doesn't match");
-    } else {
-      navigate("/Register2");
-    }
-  };
-
-  return (
-    <div className="h-screen w-full flex items-center justify-center bg-gradient-to-b from-violet-400 to-fuchsia-100">
-      <div className="max-w-md mx-auto p-6 bg-gradient-to-b from-violet-400 to-fuchsia-200 rounded-xl shadow-2xl">
-        <h1 className="text-2xl font-mono text-center pb-8">Create Your Account</h1>
-        <form onSubmit={handleSubmit}>
-          <div className="relative mb-4">
-            <div className="absolute flex items-center justify-center h-10 w-10 bg-white rounded-lg">
-              <AccountCircleIcon />
+    setUsername(e.target.value)
+  }
+        const handleEmailChange = (e) => {
+            setEmail(e.target.value);
+          };
+        
+          const handlePasswordChange = (e) => {
+            setPassword(e.target.value);
+          };
+        
+          const handleConfirmPasswordChange = (e) => {
+            setConfirmPassword(e.target.value);
+          };
+        
+          const handleSubmit = (e) => {
+            e.preventDefault();
+        
+            if (password.trim() !== confirmPassword.trim()) {
+              alert("Confirmed password doesn't match");
+            } else {
+              setCurrentSlide(1);
+            }
+          };
+          return (
+            <div className="carousel" style={{ height: "40vh" }}>
+              <div className="carousel-inner">
+                <div className={`carousel-item ${currentSlide === 0 ? "active" : ""}`}>
+                  <div className="h-screen w-full flex items-center justify-center bg-gradient-to-b from-violet-400 to-fuchfrom-violet-400 to-fuchsia-100">
+            <div className="max-w-md mx-auto p-6 bg-gradient-to-b from-violet-400 to-fuchsia-200 rounded-xl shadow-2xl">
+              <h1 className="text-2xl font-mono text-center pb-8">
+                Create Your Account
+              </h1>
+              <form onSubmit={handleSubmit}>
+                <div className="relative mb-4">
+                  <div className="absolute flex items-center justify-center h-10 w-10 bg-white rounded-lg">
+                    <AccountCircleIcon />
+                  </div>
+                  <input
+                    type="text"
+                    name="name"
+                    className="pl-12 h-10 w-full rounded-lg"
+                    placeholder="Enter username"
+                    value={username}
+                    onChange={handleUsernameChange}
+                    required
+                  />
+                </div>
+                <div className="relative mb-4">
+                  <div className="absolute flex items-center justify-center h-10 w-10 bg-white rounded-lg">
+                    <LockIcon />
+                  </div>
+                  <input
+                    type="email"
+                    name="email"
+                    className="pl-12 h-10 w-full rounded-lg"
+                    placeholder="Enter Email id"
+                    value={email}
+                    onChange={handleEmailChange}
+                    required
+                  />
+                </div>
+                <div className="relative mb-4">
+                  <div className="absolute flex items-center justify-center h-10 w-10 bg-white rounded-lg">
+                    <LockIcon />
+                  </div>
+                  <input
+                    type="password"
+                    name="password"
+                    className="pl-12 h-10 w-full rounded-lg"
+                    placeholder="Enter password"
+                    value={password}
+                    onChange={handlePasswordChange}
+                    required
+                  />
+                </div>
+                <div className="relative mb-4">
+                  <div className="absolute flex items-center justify-center h-10 w-10 bg-white rounded-lg">
+                    <LockIcon />
+                  </div>
+                  <input
+                    type="password"
+                    name="confirmPassword"
+                    className="pl-12 h-10 w-full rounded-lg"
+                    placeholder="Confirm password"
+                    value={confirmPassword}
+                    onChange={handleConfirmPasswordChange}
+                    required
+                  />
+                </div>
+                <div className="text-center">
+                  <button
+                    type="submit"
+                    className="w-12 h-12 rounded-full bg-gradient-to-r from-fuchsia-400 to-purple-400 hover:from-fuchsia-200 hover:to-purple-500 text-white"
+                  >
+                    <ArrowForwardIcon />
+                  </button>
+                </div>
+              </form>
+              <div className="mt-8 text-center">
+                <p>Already have an account?</p>
+                <button className="underline" onClick={() => navigate(-1)}>
+                  Click here
+                </button>
+              </div>
             </div>
-            <input
-              type="text"
-              name="name"
-              className="pl-12 h-10 w-full rounded-lg"
-              placeholder="Enter username"
-              value={username}
-              onChange={handleUsernameChange}
-              required
-            />
           </div>
-          <div className="relative mb-4">
-            <div className="absolute flex items-center justify-center h-10 w-10 bg-white rounded-lg">
-              <LockIcon />
-            </div>
-            <input
-              type="email"
-              name="email"
-              className="pl-12 h-10 w-full rounded-lg"
-              placeholder="Enter Email id"
-              value={email}
-              onChange={handleEmailChange}
-              required
-            />
-          </div>
-          <div className="relative mb-4">
-            <div className="absolute flex items-center justify-center h-10 w-10 bg-white rounded-lg">
-              <LockIcon />
-            </div>
-            <input
-              type="password"
-              name="password"
-              className="pl-12 h-10 w-full rounded-lg"
-              placeholder="Enter password"
-              value={password}
-              onChange={handlePasswordChange}
-              required
-            />
-          </div>
-          <div className="relative mb-4">
-            <div className="absolute flex items-center justify-center h-10 w-10 bg-white rounded-lg">
-              <LockIcon />
-            </div>
-            <input
-              type="password"
-              name="confirmPassword"
-              className="pl-12 h-10 w-full rounded-lg"
-              placeholder="Confirm password"
-              value={confirmPassword}
-              onChange={handleConfirmPasswordChange}
-              required
-            />
-          </div>
-          <div className="text-center">
-            <button
-              type="submit"
-              className="w-12 h-12 rounded-full bg-gradient-to-r from-fuchsia-400 to-purple-400 hover:from-fuchsia-200 hover:to-purple-500 text-white"
+        </div>
+        <div className={`carousel-item ${currentSlide === 1 ? "active" : ""}`}>
+          {/* <img
+            src="Back-End/pic/IMG_3943.jpg"
+            className="d-block w-100 bg-purple-300 h-64"
+            alt="Slide 2"
+          /> */}
+          <div className="h-screen w-full flex items-center justify-center bg-gradient-to-b from-violet-400 to-fuchsia-100">
+            <form
+              action="get"
+              className="max-w-md mx-auto p-6 bg-gradient-to-b from-violet-400 via-purple-200 to-fuchsia-100 rounded-xl shadow-lg"
             >
-              <ArrowForwardIcon />
-            </button>
+              <div className="flex items-center justify-center">
+                <div className="relative rounded-full h-32 w-32 bg-gradient-to-r from-violet-700 via-purple-400 to-fuchsia-500 p-1 hover:bg-gradient-to-b from-violet-700 via-purple-400 to-fuchsia-500 hover:delay-1000">
+                  <div className="absolute inset-0 flex items-center justify-center">
+                    {profilePicture ? (
+                      <img
+                        src={profilePicture}
+                        alt="Profile"
+                        className="rounded-full h-28 w-28"
+                      />
+                    ) : (
+                      <button className="rounded-full bg-slate-100 h-28 w-28">
+                        <AddPhotoAlternateIcon />
+                      </button>
+                    )}
+                  </div>
+                </div>
+              </div>
+              <input
+                type="file"
+                className="ml-4 mt-6 text-sm bg-transparent"
+                onChange={handleProfilePictureChange}
+              />
+              <div className="text-black">
+                <input
+                  type="text"
+                  name="fullname"
+                  id="fullname"
+                  placeholder="Your Full Name"
+                  className="px-3 w-full border-2 border-fuchsia-800 h-9 rounded-xl mt-8"
+                  required
+                />
+              </div>
+              <div className="mt-8">
+                <div className="bg-white border-fuchsia-800 border-2 px-3 py-1 rounded-xl h-8">
+                  <label htmlFor="gender" className="text-black">
+                    Gender:
+                  </label>
+                  <select
+                    id="gender"
+                    name="gender"
+                    className="absolute"
+                    required
+                  >
+                    <option value="male">Male</option>
+                    <option value="female">Female</option>
+                    <option value="other">Other</option>
+                  </select>
+                </div>
+                <br />
+                <div className="bg-white mt-2 border-fuchsia-800 border-2 px-2 py-1 rounded-xl h-8">
+                  <label htmlFor="dob" className="text-black">
+                    DOB
+                  </label>
+                  <input
+                    type="date"
+                    name=""
+                    id=""
+                    className="bg-transparent absolute border-1 w-24 ml-3 rounded-xl"
+                  />
+                </div>
+                <div className="bg-white mt-4 border-fuchsia-800 border-2 rounded-xl">
+                  <div className="card rounded-xl">
+                    <div className="card-header">BioData</div>
+                    <ul className="list-group list-group-flush">
+                      <li className="list-group-item underline">
+                        <input
+                          type="text"
+                          className="w-full h-8 border-1 pl-1"
+                        />
+                      </li>
+                      <li className="list-group-item underline">
+                        <input
+                          type="text"
+                          className="w-full h-8 border-1 pl-1"
+                        />
+                      </li>
+                      <li className="list-group-item underline">
+                        <input
+                          type="text"
+                          className="w-full h-8 border-1 pl-1"
+                        />
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+                <div className="flex justify-center mt-8">
+                  <input
+                    type="submit"
+                    className="w-36 h-12 rounded-full bg-gradient-to-l from-fuchsia-300 to-purple-600 hover:from-fuchsia-200 hover:to-purple-400 text-white font-bold"
+                    value="Submit"
+                  />
+                </div>
+              </div>
+            </form>
           </div>
-        </form>
-        <div className="mt-8 text-center">
-          <p>Already have an account?</p>
-          <button className="underline" onClick={() => navigate(-1)}>
-            Click here
-          </button>
         </div>
       </div>
     </div>
   );
-}
+};
+export default Register;
