e63ff2d5119cb0b9e750c7f5208726b0b7c459fb
JayP <jaypatel4099@gmail.com>
2023-07-12 17:01:34 +0530

    admin part added

--- Front_End/src/Components/Admin.jsx
+++ Front_End/src/Components/Admin.jsx
@@ -4,8 +4,12 @@ export default function Admin() {
     return (
         <>
             <div>
-                <form className="w-full max-w-lg">
-                    <div className="flex flex-wrap mb-6 -mx-3">
+                <div className="p-2 ">
+                    <div className="text-lg text-gray-700 font-bold"> Company Details</div>
+                    <hr className="px-2"/>
+                </div>
+                <form className="w-full max-w-lg mx-auto pb-5">
+                    <div className="flex flex-wrap mb-6 -mx-3 p-2">
                         <div className="w-full px-3 mb-6 md:w-1/2 md:mb-0">
                             <label className="block mb-2 text-xs font-bold tracking-wide text-gray-700 uppercase" htmlFor="grid-company-name">
                                 Company Name
@@ -20,7 +24,7 @@ export default function Admin() {
                             <input className="block w-full px-4 py-3 leading-tight text-gray-700 bg-gray-200 border border-gray-200 rounded appearance-none focus:outline-none focus:bg-white focus:border-gray-500" id="grid-company-email" type="email" placeholder="company@example.com" />
                         </div>
                     </div>
-                    <div className="flex flex-wrap mb-6 -mx-3">
+                    <div className="flex flex-wrap mb-6 -mx-3 p-2">
                         <div className="w-full px-3">
                             <label className="block mb-2 text-xs font-bold tracking-wide text-gray-700 uppercase" htmlFor="grid-image">
                                 Image
@@ -28,7 +32,7 @@ export default function Admin() {
                             <input className="block w-full px-4 py-3 mb-3 leading-tight text-gray-700 bg-gray-200 border border-gray-200 rounded appearance-none focus:outline-none focus:bg-white focus:border-gray-500" id="grid-image" type="file" />
                         </div>
                     </div>
-                    <div className="flex flex-wrap mb-6 -mx-3">
+                    <div className="flex flex-wrap mb-6 -mx-3 p-2">
                         <div className="w-full px-3">
                             <label className="block mb-2 text-xs font-bold tracking-wide text-gray-700 uppercase" htmlFor="grid-domain" >
                                 Domain
@@ -36,28 +40,39 @@ export default function Admin() {
                             <input className="block w-full px-4 py-3 mb-3 leading-tight text-gray-700 bg-gray-200 border border-gray-200 rounded appearance-none focus:outline-none focus:bg-white focus:border-gray-500" id="grid-domain" type="text" placeholder="Domain" /> 
                         </div>
                     </div>
-                    <div className="flex flex-wrap mb-6 -mx-3">
+                    <div className="flex flex-wrap mb-6 -mx-3 p-2">
                         <div className="w-full px-3">
                             <label className="block mb-2 text-xs font-bold tracking-wide text-gray-700 uppercase" htmlFor="grid-hiring">
                                 Hiring (Internship/Job)
                             </label>
-                            <input className="block w-full px-4 py-3 mb-3 leading-tight text-gray-700 bg-gray-200 border border-gray-200 rounded appearance-none focus:outline-none focus:bg-white focus:border-gray-500" id="grid-hiring" type="text" placeholder="Hiring (Internship/Job)" />
+                            <select className="block w-full px-4 py-3 mb-3 leading-tight text-gray-700 bg-gray-200 border border-gray-200 rounded appearance-none focus:outline-none focus:bg-white focus:border-gray-500" id="grid-hiring" placeholder="Hiring">
+                                <option value="internship">Internship</option>
+                                <option value="Job">Job</option>
+                            </select> 
                         </div>
                     </div>
-                    <div className="flex flex-wrap mb-6 -mx-3">
+                    <div className="flex flex-wrap mb-6 -mx-3 p-2">
                         <div className="w-full px-3">
                             <label className="block mb-2 text-xs font-bold tracking-wide text-gray-700 uppercase" htmlFor="grid-hiring">
-                                Hiring (Internship/Job)
+                            Total vaccancy : 
                             </label>
-                            <input className="block w-full px-4 py-3 mb-3 leading-tight text-gray-700 bg-gray-200 border border-gray-200 rounded appearance-none focus:outline-none focus:bg-white focus:border-gray-500" id="grid-hiring" type="text" placeholder="Hiring (Internship/Job)" />
+                            <input className="block w-full px-4 py-3 mb-3 leading-tight text-gray-700 bg-gray-200 border border-gray-200 rounded appearance-none focus:outline-none focus:bg-white focus:border-gray-500" id="grid-hiring" type="number" placeholder="100" />
                         </div>
                     </div>
-                    <div className="flex flex-wrap mb-6 -mx-3">
+                    <div className="flex flex-wrap mb-6 -mx-3 p-2">
                         <div className="w-full px-3">
                             <label className="block mb-2 text-xs font-bold tracking-wide text-gray-700 uppercase" htmlFor="grid-hiring">
-                                Hiring (Internship/Job)
+                            Company address
                             </label>
-                            <input className="block w-full px-4 py-3 mb-3 leading-tight text-gray-700 bg-gray-200 border border-gray-200 rounded appearance-none focus:outline-none focus:bg-white focus:border-gray-500" id="grid-hiring" type="text" placeholder="Hiring (Internship/Job)" />
+                            <input className="block w-full px-4 py-3 mb-3 leading-tight text-gray-700 bg-gray-200 border border-gray-200 rounded appearance-none focus:outline-none focus:bg-white focus:border-gray-500" id="grid-hiring" type="text" placeholder="Address" />
+                        </div>
+                    </div>
+                    <div className="flex flex-wrap mb-6 -mx-3 p-2">
+                        <div className="w-full px-3">
+                            <label className="block mb-2 text-xs font-bold tracking-wide text-gray-700 uppercase" htmlFor="grid-hiring">
+                            Description
+                            </label>
+                            <input className="block w-full px-4 py-3 mb-3 leading-tight text-gray-700 bg-gray-200 border border-gray-200 rounded appearance-none focus:outline-none focus:bg-white focus:border-gray-500" id="grid-hiring" type="textarea" placeholder="Description" />
                         </div>
                     </div>
                     </form>
