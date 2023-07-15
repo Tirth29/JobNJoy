b99434f7796634b5a47d47fa9d8a5a64df9764f7
JayP <jaypatel4099@gmail.com>
2023-07-15 17:58:47 +0530

    add update

--- Front_End/src/Components/AddCompany.jsx
+++ Front_End/src/Components/AddCompany.jsx
@@ -128,7 +128,7 @@ const  AddCompany = () => {
                             <input className="block w-full px-4 py-3 mb-3 leading-tight text-gray-700 bg-gray-200 border border-gray-200 rounded appearance-none focus:outline-none focus:bg-white focus:border-gray-500"
                                 id="grid-image"
                                 type="file"
-                                value={photo}
+                                // value={photo}
                                 onChange={handleProfilePictureChange} 
                                 required />
                         </div>
